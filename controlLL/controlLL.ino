979b8f0cdd7729d5ffe232d50d371d5a0916a8ee
jallen33 <josh.allen@huskers.unl.edu>
2022-12-02 21:14:55 -0600

    Add arduino file for serial communication

---
+++ controlLL/controlLL.ino
@@ -0,0 +1,60 @@
+///*
+//  AnalogReadSerial
+//
+//  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+//  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+//  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+//
+//  This example code is in the public domain.
+//
+//  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+//*/
+//
+//// the setup routine runs once when you press reset:
+//void setup() {
+//  // initialize serial communication at 9600 bits per second:
+//  Serial.begin(9600);
+//}
+//
+//// the loop routine runs over and over again forever:
+//void loop() {
+//  // read the input on analog pin 0:
+//  int sensorValue = analogRead(A0);
+//  // print out the value you read:
+//  Serial.println(sensorValue);
+//  delay(1);  // delay in between reads for stability
+//}
+
+
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-potentiometer
+ */
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin A0:
+  int analogValue = analogRead(A0);
+  // Rescale to potentiometer's voltage (from 0V to 5V):
+  float voltage = floatMap(analogValue, 0, 1023, 0, 5);
+
+  // print out the value you read:
+  Serial.print("Analog: ");
+  Serial.print(analogValue);
+  Serial.print(", Voltage: ");
+  Serial.println(voltage);
+  delay(1000);
+}
